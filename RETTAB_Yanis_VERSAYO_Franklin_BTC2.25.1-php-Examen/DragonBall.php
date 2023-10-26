40d880e80b6a9af313397e345695de7d7b5f1701
YanisRe <y.rettab@ecole-ipssi.net>
2023-10-26 14:16:57 +0200

    avancée

--- RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
+++ RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
@@ -59,75 +59,184 @@ class Personnage{
             return est_vivant();
         }
     }
+
+    public function choixCamp(){
+
+        if($this->est_vilain = $true){
+
+        }
+        else{
+            $adversaire = $vilain($i);
+        }
+
+        $this->est_vilain = $false;
+        echo "Veuillez choisir votre camp : h pour héros ou v pour vilain." . PHP_EOL;
+        $choix = readline();
+        
+        if($choix === "h"){
+            $this->est_vilain = false;
+            echo "Veuillez entrer le nom de votre personnage." . PHP_EOL;
+            $nom = readline();
+                $hero($i)= new Heros($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+                echo "Vous avez choisi le camp des héros !" . PHP_EOL;
+                return $hero($i);
+        }
+        else if ($choix === "v"){
+            $this->est_vilain = true;
+            $adversaire = $hero($i);
+            echo "Veuillez entrer le nom de votre personnage." . PHP_EOL;
+            $nom = readline();
+            $vilain($i)= new Vilains($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+                echo "Vous avez choisi le camp des vilains !" . PHP_EOL;
+                return $vilain($i);
+        }
+        else{
+            echo "Veuillez choisir un camp valide." . PHP_EOL;
+            choixCamp();
+        }
+    }
 }
+
+
+
 class Heros extends Personnage{
     
-    private $adversaire;
+    private $adversaire = $vilain($i);
     private $nom;
-    private $vie;
-    private $puissance_attaque;
-    private $degats_subis;
+    private $vie = 400;
+    private $puissance_attaque = 20;
+    private $degats_subis = $this->prendre_degats($this->$vilain($i)->puissance_attaque);
     
     
     public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire){
         parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
     }
     
+    $hero = [$hero1, $hero2, $hero3, $hero4, $hero5, $hero6, $hero7, $hero8, $hero9, $hero10];
+
+    $hero1 = new Heros("Goku", 100, 20, 10, $vilain($i));
+    $hero2 = new Heros("Vegeta", 100, 20, 10, $vilain($i));
+    $hero3 = new Heros("Gohan", 100, 20, 10, $vilain($i));
+    $hero4 = new Heros("Trunks", 100, 20, 10, $vilain($i));
+    $hero5 = new Heros("Goten", 100, 20, 10, $vilain($i));
+    $hero6 = new Heros("Piccolo", 100, 20, 10, $vilain($i));
+    $hero7 = new Heros("Krilin", 100, 20, 10, $vilain($i));
+    $hero8 = new Heros("C-18", 100, 20, 10, $vilain($i));
+    $hero9 = new Heros("Tenshinhan", 100, 20, 10, $vilain($i));
+    $hero10 = new Heros("C-17", 100, 20, 10, $vilain($i));
+
+
 }
 
 class Vilains extends Personnage{
     
-    private $adversaire;
+    private $adversaire = $hero($i);
     private $nom;
-    private $vie;
-    private $puissance_attaque;
-    private $degats_subis;
+    private $vie = 480;
+    private $puissance_attaque = 10;
+    private $degats_subis = $this->prendre_degats($this->$hero($i)->puissance_attaque);
     
     
     public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire){
         parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
     }
     
+    $vilain = [$vilain1, $vilain2, $vilain3, $vilain4, $vilain5, $vilain6, $vilain7, $vilain8, $vilain9, $vilain10];
+
+    $vilain1 = new Vilains("Radditz", 100, 20, 10, $hero($i));
+    $vilain2 = new Vilains("Freezer", 100, 20, 10, $hero($i));
+    $vilain3 = new Vilains("Cell", 100, 20, 10, $hero($i));
+    $vilain4 = new Vilains("Janemba", 100, 20, 10, $hero($i));
+    $vilain5 = new Vilains("Buu", 100, 20, 10, $hero($i));
+    $vilain6 = new Vilains("Dr. Gero", 100, 20, 10, $hero($i));
+    $vilain7 = new Vilains("Zamasu", 100, 20, 10, $hero($i));
+    $vilain8 = new Vilains("Cell Jr.", 100, 20, 10, $hero($i));
+    $vilain9 = new Vilains("Saibaiman", 100, 20, 10, $hero($i));
+    $vilain10 = new Vilains("Li Shenron", 100, 20, 10, $hero($i));
+
 }
 
 class combat extends Personnage{
-
+    
     private $adversaire;
     private $nom;
     private $vie;
     private $puissance_attaque;
     private $degats_subis;
+
 
     public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire){
         parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
     }
 
     public function systeme_de_combat(){
-        while($this->est_vivant() && $this->adversaire->est_vivant()){
+        $this->adversaire = $adversaire;
+        $this->nom = $nom;
+        $this->vie = $vie;
+        $this->puissance_attaque = $puissance_attaque;
+        $this->degats_subis = $degats_subis;
+        $this->adversaire = $adversaire;
+
+
+        //menu();
+        //synopsis(); if play puis choixCamp();
+
+        choixCamp();
+        while($this->vie > 0 && $this->adversaire->vie > 0){
+                        
             $this->attaque($this->adversaire);
             $this->adversaire->attaque($this);
 
-            $this->attaque($this->adversaire) = $this->puissance_attaque;
-            $this->adversaire->attaque($this) = $this->puissance_attaque;
+            $this->attaque($this->adversaire) = $this->prendre_degats($this->degats_subis);
+            $this->adversaire->attaque($this) = $this->adversaire->prendre_degats($this->degats_subis);
+
+            est_mort();
 
 
+            //est_vivant();
+            
         }
     }
 }
 
-class IA extends Personnage {
+
+class IA extends Personnage{
+
     private $adversaire;
     private $nom;
     private $vie;
     private $puissance_attaque;
     private $degats_subis;
-    private boolval $est_vilain
+    private boolval $est_vilain;
 
 
 }
 
-class Jeu extends combat {
+class Jeu extends combat{
     
+        private $adversaire;
+        private $nom;
+        private $vie;
+        private $puissance_attaque;
+        private $degats_subis;
+        private $est_vilain;
+        private $est_heros;
+    
+        public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain, $est_heros){
+            parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+            $this->est_vilain = $est_vilain;
+            $this->est_heros = $est_heros;
+        }
+    
+        public function systeme_de_jeu(){
+            $this->adversaire = $adversaire;
+            $this->nom = $nom;
+            $this->vie = $vie;
+            $this->puissance_attaque = $puissance_attaque;
+            $this->degats_subis = $degats_subis;
+            $this->adversaire = $adversaire;
+            $this->est_vilain = $est_vilain;
+            $this->est_heros = $est_heros;
+            
+        }
 }
-
-?>
