5de49518fd0ce2f32e75e860986e5bc1e3e36987
YanisRe <y.rettab@ecole-ipssi.net>
2023-10-26 14:18:30 +0200

    CreationTestPage

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php 
+public function menu(){
+    echo "Bienvenue dans le monde de Dragon Ball ! " . PHP_EOL;
+    echo "Veuillez choisir une action : p pour jouer ou q pour quitter." . PHP_EOL;
+    $action = readline();
+    switch ($action):
+        case "p":
+            synopsis();
+            break;
+        case "q":
+            echo "Vous avez quitté le jeu, à bientôt !";
+            break;
+        default:
+            echo "Veuillez choisir une action valide." . PHP_EOL;
+            $action = readline();
+            break;
+    endswitch;
+}
