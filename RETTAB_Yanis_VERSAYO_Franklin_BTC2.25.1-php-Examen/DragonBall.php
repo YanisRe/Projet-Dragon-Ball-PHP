13cc7db0212e53187f12c3b5db688394162244b1
SmiledPolecat34 <versayo03@gmail.com>
2023-10-26 12:12:10 +0200

    Création class perso et extends + variables

---
+++ RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
@@ -0,0 +1,19 @@
+<?php
+
+class Personnage{
+
+    protected $nom;
+    protected $vie;
+    protected $puissance_attaque;
+}
+
+class Heros extends Personnage{
+
+}
+
+class Vilains extends Personnage{
+
+}
+
+
+?>
