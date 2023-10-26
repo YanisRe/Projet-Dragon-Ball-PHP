2d1f129976e6c0f0f0e4d2a792ec04550d650827
YanisRe <y.rettab@ecole-ipssi.net>
2023-10-26 16:42:33 +0200

    fin de jour

--- RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
+++ RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
@@ -100,47 +100,33 @@ class Personnage{
     }
 }
 
-    $vilain1 = new Vilains("Radditz", 100, 20, 10, $hero($i));
-    $vilain2 = new Vilains("Freezer", 100, 20, 10, $hero($i));
-    $vilain3 = new Vilains("Cell", 100, 20, 10, $hero($i));
-    $vilain4 = new Vilains("Janemba", 100, 20, 10, $hero($i));
-    $vilain5 = new Vilains("Buu", 100, 20, 10, $hero($i));
-    $vilain6 = new Vilains("Dr. Gero", 100, 20, 10, $hero($i));
-    $vilain7 = new Vilains("Zamasu", 100, 20, 10, $hero($i));
-    $vilain8 = new Vilains("Cell Jr.", 100, 20, 10, $hero($i));
-    $vilain9 = new Vilains("Saibaiman", 100, 20, 10, $hero($i));
-    $vilain10 = new Vilains("Li Shenron", 100, 20, 10, $hero($i));
-
-$vilain = [$vilain1, $vilain2, $vilain3, $vilain4, $vilain5, $vilain6, $vilain7, $vilain8, $vilain9, $vilain10];
-
-    
-$hero1 = new Heros("Goku", 100, 20, 10, $vilain($i));
-$hero2 = new Heros("Vegeta", 100, 20, 10, $vilain($i));
-$hero3 = new Heros("Gohan", 100, 20, 10, $vilain($i));
-$hero4 = new Heros("Trunks", 100, 20, 10, $vilain($i));
-$hero5 = new Heros("Goten", 100, 20, 10, $vilain($i));
-$hero6 = new Heros("Piccolo", 100, 20, 10, $vilain($i));
-$hero7 = new Heros("Krilin", 100, 20, 10, $vilain($i));
-$hero8 = new Heros("C-18", 100, 20, 10, $vilain($i));
-$hero9 = new Heros("Tenshinhan", 100, 20, 10, $vilain($i));
-$hero10 = new Heros("C-17", 100, 20, 10, $vilain($i));
-
-
-
-$hero = [$hero1, $hero2, $hero3, $hero4, $hero5, $hero6, $hero7, $hero8, $hero9, $hero10];
-
-
 
 
 class Heros extends Personnage{
     
     protected boolval $est_vilain;
-    protected $vilain;
-    protected $adversaire;
-    protected $nom;
-    protected $vie = 400;
-    protected $puissance_attaque = 20;
-    protected $degats_subis;
+    $vilain;
+    $adversaire;
+    $nom;
+    $vie = 400;
+    $puissance_attaque = 20;
+    $degats_subis;
+
+    $hero1 = new Heros("Goku", 100, 20, 10, $vilain($i));
+    $hero2 = new Heros("Vegeta", 100, 20, 10, $vilain($i));
+    $hero3 = new Heros("Gohan", 100, 20, 10, $vilain($i));
+    $hero4 = new Heros("Trunks", 100, 20, 10, $vilain($i));
+    $hero5 = new Heros("Goten", 100, 20, 10, $vilain($i));
+    $hero6 = new Heros("Piccolo", 100, 20, 10, $vilain($i));
+    $hero7 = new Heros("Krilin", 100, 20, 10, $vilain($i));
+    $hero8 = new Heros("C-18", 100, 20, 10, $vilain($i));
+    $hero9 = new Heros("Tenshinhan", 100, 20, 10, $vilain($i));
+    $hero10 = new Heros("C-17", 100, 20, 10, $vilain($i));
+
+
+
+    protected $hero = [$hero1, $hero2, $hero3, $hero4, $hero5, $hero6, $hero7, $hero8, $hero9, $hero10];
+
     
     
     public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain){
@@ -164,12 +150,26 @@ class Heros extends Personnage{
 
 class Vilains extends Personnage{
     
-    protected boolval $est_vilain;
-    protected $adversaire;
-    protected $nom;
-    protected $vie = 480;
-    protected $puissance_attaque = 10;
-    protected $degats_subis;
+    boolval $est_vilain;
+    $adversaire;
+    $nom;
+    $vie = 480;
+    $puissance_attaque = 10;
+    $degats_subis;
+
+    $vilain1 = new Vilains("Radditz", 100, 20, 10, $hero($i));
+    $vilain2 = new Vilains("Freezer", 100, 20, 10, $hero($i));
+    $vilain3 = new Vilains("Cell", 100, 20, 10, $hero($i));
+    $vilain4 = new Vilains("Janemba", 100, 20, 10, $hero($i));
+    $vilain5 = new Vilains("Buu", 100, 20, 10, $hero($i));
+    $vilain6 = new Vilains("Dr. Gero", 100, 20, 10, $hero($i));
+    $vilain7 = new Vilains("Zamasu", 100, 20, 10, $hero($i));
+    $vilain8 = new Vilains("Cell Jr.", 100, 20, 10, $hero($i));
+    $vilain9 = new Vilains("Saibaiman", 100, 20, 10, $hero($i));
+    $vilain10 = new Vilains("Li Shenron", 100, 20, 10, $hero($i));
+
+    protected $vilain = [$vilain1, $vilain2, $vilain3, $vilain4, $vilain5, $vilain6, $vilain7, $vilain8, $vilain9, $vilain10];
+
     
     public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain){
         parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain);
