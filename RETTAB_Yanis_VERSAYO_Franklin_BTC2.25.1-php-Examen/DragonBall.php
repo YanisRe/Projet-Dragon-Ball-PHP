822e04fc53d9d38428014b9f4f9cc322fb5c1b36
SmiledPolecat34 <versayo03@gmail.com>
2023-10-26 12:20:53 +0200

    Ajouts de méthodes

--- RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
+++ RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
@@ -5,28 +5,67 @@ class Personnage{
     protected $nom;
     protected $vie;
     protected $puissance_attaque;
+    protected $degats_subis;
 
     public function __construct($nom, $vie, $puissance_attaque){
         $this->nom = $nom;
         $this->vie = $vie;
         $this->puissance_attaque = $puissance_attaque;
-
     }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function getVie(){
+        return $this->vie;
+    }
+
+    public function getPuissance_attaque(){
+        return $this->puissance_attaque;
+    }
+
+    public function getDegats_subis(){
+        return $this->degats_subis;
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+
+    public function attaque($adversaire){
+        $adversaire->vie -= $this->puissance_attaque;
+    }
+
+    public function prendre_degats($degats_subis){
+        $this->vie -= $degats_subis;
+    }
+
+    public function est_vivant(){
+        if($this->vie > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function est_mort(){
+        if($this->vie <= 0){
+            return true;
+    }
+
+
 }
 
 class Heros extends Personnage{
 
-    public function __construct($nom, $vie, $puissance_attaque){
-        parent::__construct($nom, $vie, $puissance_attaque);
-    }
+
     
 }
 
 class Vilains extends Personnage{
     
-    public function __construct($nom, $vie, $puissance_attaque){
-        parent::__construct($nom, $vie, $puissance_attaque);
-    }
+
 }
 
 
