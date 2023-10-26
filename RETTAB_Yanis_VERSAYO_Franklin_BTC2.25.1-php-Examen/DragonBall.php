50d10ae3fb9dd19fe8273ff5bd8c01889c6b0122
SmiledPolecat34 <versayo03@gmail.com>
2023-10-26 12:14:02 +0200

    Changement1

--- RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
+++ RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBall.php
@@ -5,14 +5,28 @@ class Personnage{
     protected $nom;
     protected $vie;
     protected $puissance_attaque;
+
+    public function __construct($nom, $vie, $puissance_attaque){
+        $this->nom = $nom;
+        $this->vie = $vie;
+        $this->puissance_attaque = $puissance_attaque;
+
+    }
 }
 
 class Heros extends Personnage{
 
+    public function __construct($nom, $vie, $puissance_attaque){
+        parent::__construct($nom, $vie, $puissance_attaque);
+    }
+    
 }
 
 class Vilains extends Personnage{
-
+    
+    public function __construct($nom, $vie, $puissance_attaque){
+        parent::__construct($nom, $vie, $puissance_attaque);
+    }
 }
 
 
