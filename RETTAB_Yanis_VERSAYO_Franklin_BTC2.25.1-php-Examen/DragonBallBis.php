9d40b02f46c52e9db410452a90c344cbfa037b90
SmiledPolecat34 <versayo03@gmail.com>
2023-10-27 13:20:35 +0200

    New creation about the game since yesterday 6pm.

---
+++ RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/DragonBallBis.php
@@ -0,0 +1,420 @@
+<?php
+
+class Personnage {
+    protected $nom;
+    protected $vie;
+    protected $puissance_attaque;
+    protected $degats_subis;
+    protected $est_vilain;
+    protected $inventaire;
+
+    public function __construct($nom, $vie, $puissance_attaque, $est_vilain = false) {
+        $this->nom = $nom;
+        $this->vie = $vie;
+        $this->puissance_attaque = $puissance_attaque;
+        $this->degats_subis = 0;
+        $this->est_vilain = $est_vilain;
+        $this->inventaire = [];
+    }
+
+    public function prendreDegats($degats) {
+        $this->degats_subis += $degats;
+        $this->vie -= $degats;
+        if ($this->vie <= 0) {
+            $this->vie = 0;
+        }
+    }
+
+    public function getPersonnageData() {
+        return [
+            'nom' => $this->nom,
+            'vie' => $this->vie,
+            'puissance_attaque' => $this->puissance_attaque,
+            'degats_subis' => $this->degats_subis,
+            'est_vilain' => $this->est_vilain,
+            'inventaire' => $this->inventaire,
+        ];
+    }
+    
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function getVie() {
+        return $this->vie;
+    }
+
+    public function getPuissance_attaque() {
+        return $this->puissance_attaque;
+    }
+
+    public function getDegats_subis() {
+        return $this->degats_subis;
+    }
+
+    public function attaque($adversaire) {
+        $adversaire->prendreDegats($this->puissance_attaque);
+    }
+
+    public function getPlayerStats() {
+        echo "Statistiques du joueur : " . PHP_EOL;
+        echo "Nom : " . $this->getNom() . PHP_EOL;
+        echo "Points de vie : " . $this->getVie() . PHP_EOL;
+        echo "Puissance d'attaque : " . $this->getPuissance_attaque() . PHP_EOL;
+        if (!empty($this->inventaire)) {
+            echo "Inventaire : " . implode(", ", $this->inventaire) . PHP_EOL;
+        }
+    }
+
+    public function estVivant() {
+        return $this->vie > 0;
+    }
+
+    public function estMort() {
+        return $this->vie <= 0;
+    }
+
+    public function ajouterInventaire($objet) {
+        $this->inventaire[] = $objet;
+    }
+
+    public function utiliserObjet($objet) {
+        if (in_array($objet, $this->inventaire)) {
+            if ($objet === "potion") {
+                $this->vie += 20;
+                echo $this->nom . " a utilisé une potion et a récupéré 20 points de vie." . PHP_EOL;
+            }
+            $index = array_search($objet, $this->inventaire);
+            if ($index !== false) {
+                unset($this->inventaire[$index]);
+            }
+        } else {
+            echo $this->nom . " n'a pas cet objet dans son inventaire." . PHP_EOL;
+        }
+    }
+}
+
+class Heros extends Personnage {
+    public function __construct($nom) {
+        parent::__construct($nom, 300, 20);
+    }
+
+    public function estVivant() {
+        return $this->vie > 0;
+    }
+
+    public function setPersonnageData($data) {
+        $this->nom = $data['nom'];
+        $this->vie = $data['vie'];
+        $this->puissance_attaque = $data['puissance_attaque'];
+        $this->degats_subis = $data['degats_subis'];
+        $this->est_vilain = $data['est_vilain'];
+        //$this->inventaire = $data['inventaire'];
+    }
+    
+
+    public function attaqueSpeciale($adversaire) {
+        if ($this->vie >= 30) {
+            $this->vie -= 30;
+            $adversaire->prendreDegats(40);
+            echo $this->nom . " a lancé l'attaque spéciale Kamehameha!" . PHP_EOL;
+        } else {
+            echo $this->nom . " n'a pas suffisamment de vie pour utiliser l'attaque spéciale." . PHP_EOL;
+        }
+    }
+}
+
+class Vilains extends Personnage {
+    public function __construct($nom) {
+        parent::__construct($nom, 100, 20, true);
+    }
+
+    public function estVivant() {
+        return $this->vie > 0;
+    }
+
+    public function setPersonnageData($data) {
+        $this->nom = $data['nom'];
+        $this->vie = $data['vie'];
+        $this->puissance_attaque = $data['puissance_attaque'];
+        $this->degats_subis = $data['degats_subis'];
+        $this->est_vilain = $data['est_vilain'];
+        $this->inventaire = $data['inventaire'];
+    }
+    
+    public function attaqueSpeciale($adversaire) {
+        if ($this->vie >= 20) {
+            $this->vie -= 20;
+            $adversaire->prendreDegats(30);
+            echo $this->nom . " a lancé l'attaque spéciale Vilain!" . PHP_EOL;
+        } else {
+            echo $this->nom . " n'a pas suffisamment de vie pour utiliser l'attaque spéciale." . PHP_EOL;
+        }
+    }
+}
+
+class Combat {
+    
+    public function afficherStatistiques($personnage) {
+        echo $personnage->getNom() . " - Points de vie : " . $personnage->getVie() . " - Puissance d'attaque : " . $personnage->getPuissance_attaque() . PHP_EOL;
+    }
+
+    public function afficherResultatCombat($heros, $vilain) {
+        if ($heros->estMort()) {
+            echo $heros->getNom() . " a été vaincu par " . $vilain->getNom() . "!" . PHP_EOL;
+        } elseif ($vilain->estMort()) {
+            echo $vilain->getNom() . " a été vaincu par " . $heros->getNom() . "!" . PHP_EOL;
+        }
+    }
+
+    public function combat($heros, $vilain) {
+        while ($heros->estVivant() && $vilain->estVivant()) {
+            $this->afficherStatistiques($heros);
+            $this->afficherStatistiques($vilain);
+
+            echo "Actions possibles : (1) Attaque normale, (2) Attaque spéciale, (3) Utiliser objet, (4) Fuir" . PHP_EOL;
+            $action = readline("Choisissez une action (1/2/3/4) : ");
+
+            switch ($action) {
+                case "1":
+                    $heros->attaque($vilain);
+                    echo $heros->getNom() . " a attaqué " . $vilain->getNom() . " avec une attaque normale!" . PHP_EOL;
+                    break;
+                case "2":
+                    $heros->attaqueSpeciale($vilain);
+                    break;
+                // case "3":
+                //     echo "Inventaire de " . $heros->getNom() . ": " . implode(", ", $heros->inventaire) . PHP_EOL;
+                //     $objet = readline("Choisissez un objet de l'inventaire : ");
+                //     $heros->utiliserObjet($objet);
+                //     break;
+                case "4":
+                    echo $heros->getNom() . " a fui le combat." . PHP_EOL;
+                    return;
+                default:
+                    echo "Action non valide." . PHP_EOL;
+                    break;
+            }
+
+            if ($vilain->estVivant()) {
+                $vilain->attaque($heros);
+                echo $vilain->getNom() . " a attaqué " . $heros->getNom() . " avec une attaque normale!" . PHP_EOL;
+            }
+            $this->afficherStatistiques($vilain);
+        }
+
+        $this->afficherResultatCombat($heros, $vilain);
+        if ($heros->estVivant()) {
+            $heros->prendreDegats(-10);
+            echo $heros->getNom() . " a débloqué une nouvelle attaque : Kamehameha!" . PHP_EOL;
+        }
+    }
+}
+
+
+class DragonBallGame {
+    private $filename;
+    private $player;
+    private $defeatedEnemies = [];
+
+    public function __construct($filename, $player) {
+        $this->filename = $filename;
+        $this->player = $player;
+    }
+
+    public function loadGameState() {
+        if (file_exists($this->filename)) {
+            $gameStateJSON = file_get_contents($this->filename);
+            $gameState = json_decode($gameStateJSON, true);
+    
+            
+            $playerData = $gameState['player'];
+            if ($playerData['est_vilain']) {
+                $player = new Vilains($playerData['nom']);
+            } else {
+                $player = new Heros($playerData['nom']);
+            }
+    
+            $player->setPersonnageData($playerData);
+    
+            $this->player = $player;
+            $this->defeatedEnemies = $gameState['defeatedEnemies'];
+    
+            echo "Chargement de la sauvegarde effectué." . PHP_EOL;
+        }
+        else {
+            echo "Aucune sauvegarde trouvée." . PHP_EOL;
+        }
+    }
+    
+
+    public function saveGameState($player) {
+    $gameState = [
+        'player' => [
+            'nom' => $player->getNom(),
+            'vie' => $player->getVie(),
+            'puissance_attaque' => $player->getPuissance_attaque(),
+            'degats_subis' => $player->getDegats_subis(),
+            'est_vilain' => $player instanceof Vilains,
+            //'inventaire' => $player->inventaire,
+        ],
+        'defeatedEnemies' => $this->defeatedEnemies,
+    ];
+
+    $gameStateJSON = json_encode($gameState);
+    file_put_contents($this->filename, $gameStateJSON);
+    echo "Sauvegarde effectuée avec succès." . PHP_EOL;
+}
+
+public function delGameState() {
+    if (file_exists($this->filename)) {
+        unlink($this->filename);
+        echo "Sauvegarde supprimée avec succès." . PHP_EOL;
+    } else {
+        echo "Aucune sauvegarde trouvée à supprimer." . PHP_EOL;
+    }
+}
+    
+
+    public function getPlayerStats() {
+        if ($this->player !== null && $this->player instanceof Personnage) {
+            $this->player->getPlayerStats();
+        } else {
+            echo "Aucun joueur n'a été chargé." . PHP_EOL;
+        }
+    }
+
+    public function setPlayer($player) {
+        $this->player = $player;
+    }
+
+    public function addDefeatedEnemy($enemyName) {
+        $this->defeatedEnemies[] = $enemyName;
+    }
+
+    public function getDefeatedEnemies() {
+        return $this->defeatedEnemies;
+    }
+
+    public function getPlayer() {
+        return $this->player;
+    }
+}
+
+$game = new DragonBallGame('dragon_ball_save.json', null);
+
+while (true) {
+    echo "MENU :" . PHP_EOL;
+    echo "1. Lancer une nouvelle partie" . PHP_EOL;
+    echo "2. Charger une partie" . PHP_EOL;
+    echo "3. Statistiques des ennemis tués" . PHP_EOL;
+    echo "4. Quitter" . PHP_EOL;
+
+    $choix = readline("Faites votre choix (1/2/3/4) : ");
+
+    switch ($choix) {
+        case '1':
+            $i = 1;
+            $game->delGameState();
+        
+
+            echo "Choisissez un camp : (H) Héros ou (V) Vilain" . PHP_EOL;
+            $choixCamp = strtolower(readline("Entrez votre choix (H/V) : "));
+            
+            if ($choixCamp === 'h' || $choixCamp === 'H') {
+                
+                $nom = readline("Entrez le nom de votre personnage : ");
+                $player = new Heros($nom);
+                echo "Vous avez créé un héros nommé $nom." . PHP_EOL;
+            } elseif ($choixCamp === 'v') {
+                
+                $nom = readline("Entrez le nom de votre personnage : ");
+                $player = new Vilains($nom);
+                echo "Vous avez créé un vilain nommé $nom." . PHP_EOL;
+            } else {
+                echo "Camp non valide. Choisissez h pour héros ou v pour vilain." . PHP_EOL;
+                break;
+            }
+        
+            
+            $game = new DragonBallGame('dragon_ball_save.json', $player);
+            $game->loadGameState(); // Charge l'état du jeu au démarrage
+
+        
+            
+            $game = new DragonBallGame('dragon_ball_save.json', $player);
+            $game->loadGameState(); // Charge l'état du jeu au démarrage
+        
+            
+            $availableVilains = [
+                new Vilains("Freezer"),
+                new Vilains("Cell"),
+                new Vilains("Majin Buu"),
+                new Vilains("Raditz"),
+            ];
+            $vilain = $availableVilains[array_rand($availableVilains)];
+        
+            $combat = new Combat();
+            $combat->combat($player, $vilain);
+        
+            $game->addDefeatedEnemy($vilain->getNom());
+            $game->saveGameState($player);
+        
+            $game->getPlayerStats($player);
+            $i += 1;
+        
+            break;
+
+            case '2':
+            
+                $game->loadGameState();
+                $player = $game->getPlayer();
+            
+                if ($player !== null) {
+                    echo "Partie chargée." . PHP_EOL;
+            
+                    $vilain = new Vilains("Raditz");
+            
+                    $combat = new Combat();
+                    $combat->combat($player, $vilain);
+            
+                    $game->addDefeatedEnemy($vilain->getNom());
+                    $game->saveGameState($player);
+            
+                    $game->getPlayerStats($player);
+                } else {
+                    echo "Aucune partie sauvegardée trouvée." . PHP_EOL;
+                }
+                break;
+            
+                case '3':
+                    $i = 1;
+            
+            $defeatedEnemies = $game->getDefeatedEnemies();
+            if (!empty($defeatedEnemies)) {
+                echo "Statistiques des ennemis tués :" . PHP_EOL;
+                foreach ($defeatedEnemies as $enemy) {
+                    echo "Combat n°$i - $enemy" . PHP_EOL;
+                }
+            } else {
+                echo "Aucun ennemi vaincu pour le moment." . PHP_EOL;
+            }
+            break;
+
+
+            case '4':
+                // Règles du jeu
+                echo ""
+                break;
+        case '5':
+            //
+            exit();
+            break;
+
+        default:
+            echo "Choix non valide. Veuillez choisir une option valide (1/2/3/4)." . PHP_EOL;
+            break;
+    }
+}
+?>
+// Path: RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/README.md
---
+++ RETTAB_Yanis_VERSAYO_Franklin_BTC2.25.1-php-Examen/Try.php
@@ -0,0 +1,362 @@
+<?php
+
+class Personnage{
+
+    protected $nom;
+    protected $vie;
+    protected $puissance_attaque;
+    protected $degats_subis;
+    private boolval $est_vilain;
+
+
+    public function __construct($nom, $vie, $puissance_attaque){
+        $this->nom = $nom;
+        $this->vie = $vie;
+        $this->puissance_attaque = $puissance_attaque;
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function getVie(){
+        return $this->vie;
+    }
+
+    public function getPuissance_attaque(){
+        return $this->puissance_attaque;
+    }
+
+    public function getDegats_subis(){
+        return $this->degats_subis;
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+
+    public function attaque($adversaire){
+        $adversaire->vie -= $this->puissance_attaque;
+    }
+
+    public function prendre_degats($degats_subis){
+        $this->vie -= $degats_subis;
+    }
+
+    public function est_vivant(){
+        if($this->vie > 0){
+            echo "Il te reste ".$this->vie." points de vie.";
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function est_mort(){
+        if($this->vie <= 0){
+            echo "Tu es mort.";
+            return true;
+        }
+        else{
+            return est_vivant();
+        }
+    }
+
+    public function choixCamp(){
+
+        if($this->est_vilain = $true){
+
+        }
+        else{
+            $adversaire = $vilain($i);
+        }
+
+        $this->est_vilain = $false;
+        echo "Veuillez choisir votre camp : h pour héros ou v pour vilain." . PHP_EOL;
+        $choix = readline();
+        
+        if($choix === "h"){
+            $this->est_vilain = false;
+            echo "Veuillez entrer le nom de votre personnage." . PHP_EOL;
+            $nom = readline();
+            $hero = new Heros($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+            echo "Vous avez choisi le camp des héros !" . PHP_EOL;
+            return $hero($i);
+        }
+        else if ($choix === "v"){
+            $this->est_vilain = true;
+            $adversaire = $hero($i);
+            echo "Veuillez entrer le nom de votre personnage." . PHP_EOL;
+            $nom = readline();
+            $vilain = new Vilains($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+            echo "Vous avez choisi le camp des vilains !" . PHP_EOL;
+            return $vilain($i);
+        }
+        else{
+            echo "Veuillez choisir un camp valide." . PHP_EOL;
+            choixCamp();
+        }
+        affectationEnnemi();
+    }
+}
+
+
+
+class Heros extends Personnage{
+    
+    protected boolval $est_vilain;
+    protected $vilain;
+    protected $adversaire;
+    protected $nom;
+    protected $vie = 400;
+    protected $puissance_attaque = 20;
+    protected $degats_subis;
+
+    $hero1 = new Heros("Goku", 100, 20, 10, $vilain);
+    $hero2 = new Heros("Vegeta", 100, 20, 10, $vilain);
+    $hero3 = new Heros("Gohan", 100, 20, 10, $vilain);
+    $hero4 = new Heros("Trunks", 100, 20, 10, $vilain);
+    $hero5 = new Heros("Goten", 100, 20, 10, $vilain);
+    $hero6 = new Heros("Piccolo", 100, 20, 10, $vilain);
+    $hero7 = new Heros("Krilin", 100, 20, 10, $vilain);
+    $hero8 = new Heros("C-18", 100, 20, 10, $vilain);
+    $hero9 = new Heros("Tenshinhan", 100, 20, 10, $vilain);
+    $hero10 = new Heros("C-17", 100, 20, 10, $vilain);
+
+
+
+    protected $hero = [$hero1, $hero2, $hero3, $hero4, $hero5, $hero6, $hero7, $hero8, $hero9, $hero10];
+
+    
+    
+    public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain){
+        parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain);
+        $this->est_vilain = false;
+    }
+
+    public function affectationEnnemi(){
+
+        $vilain = [$vilain1, $vilain2, $vilain3, $vilain4, $vilain5, $vilain6, $vilain7, $vilain8, $vilain9, $vilain10];
+        $random = rand(0, 9);
+        $this->adversaire = $vilain[$random];
+
+        $this->prendre_degats($this->adversaire->puissance_attaque);
+        $this->adversaire->prendre_degats($this->puissance_attaque);
+        
+    }   
+}
+
+
+
+class Vilains extends Personnage{
+    
+    boolval $est_vilain;
+    $adversaire;
+    $nom;
+    $vie = 480;
+    $puissance_attaque = 10;
+    $degats_subis;
+
+    $vilain1 = new Vilains("Radditz", 100, 20, 10, $hero($i));
+    $vilain2 = new Vilains("Freezer", 100, 20, 10, $hero($i));
+    $vilain3 = new Vilains("Cell", 100, 20, 10, $hero($i));
+    $vilain4 = new Vilains("Janemba", 100, 20, 10, $hero($i));
+    $vilain5 = new Vilains("Buu", 100, 20, 10, $hero($i));
+    $vilain6 = new Vilains("Dr. Gero", 100, 20, 10, $hero($i));
+    $vilain7 = new Vilains("Zamasu", 100, 20, 10, $hero($i));
+    $vilain8 = new Vilains("Cell Jr.", 100, 20, 10, $hero($i));
+    $vilain9 = new Vilains("Saibaiman", 100, 20, 10, $hero($i));
+    $vilain10 = new Vilains("Li Shenron", 100, 20, 10, $hero($i));
+
+    protected $vilain = [$vilain1, $vilain2, $vilain3, $vilain4, $vilain5, $vilain6, $vilain7, $vilain8, $vilain9, $vilain10];
+
+    
+    public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain){
+        parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain);
+        $this->est_vilain = true;
+    }
+    
+    public function affectationEnnemi(){
+
+        $hero = [$hero1, $hero2, $hero3, $hero4, $hero5, $hero6, $hero7, $hero8, $hero9, $hero10];
+        $random = rand(0, 9);
+        $this->adversaire = $hero[$random];
+
+        $this->prendre_degats($this->adversaire->puissance_attaque);
+        $this->adversaire->prendre_degats($this->puissance_attaque);
+        
+    } 
+    
+}
+
+class combat extends Personnage{
+    
+    protected $adversaire;
+    protected $nom;
+    protected $vie;
+    protected $puissance_attaque;
+    protected $degats_subis;
+
+
+    public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire){
+        parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+    }
+
+    public function enJeu(){
+            
+        echo "Veuillez choisir une action : c pour combattre ou q pour quitter." . PHP_EOL;
+        
+        $action = readline();
+        switch ($action){
+            case "c":
+                while($this->vie > 0 && $this->adversaire->vie > 0){
+                    
+                    est_mort();
+
+                    echo "Veuillez choisir une action : a pour attaquer, t pour transformation, et f pour essayer de prendre la fuite." . PHP_EOL;
+                    $decision = readline();
+                    switch ($decision){
+                        case "a":
+                            echo "Vous avez choisi d'attaquer." . PHP_EOL . "Veuillez choisir entre une attaque normale (n) ou une attaque spéciale (s)." . PHP_EOL;
+                            $attaque = readline();
+                            switch ($attaque){
+                                case "n":
+                                    echo "Vous avez choisi l'option attaque normale." . PHP_EOL;
+                                    $this->attaque($this->adversaire);
+                                    //
+                                    break;
+                                case "s":
+                                    echo "Vous avez choisi l'option attaque spéciale." . PHP_EOL;
+                                    $attaque_speciale = readline();
+                                    switch ($attaque_speciale){
+                                        case 1:
+                                            echo "Vous avez choisi l'attaque spéciale Kienzan." . PHP_EOL;
+                                            $this->attaque($this->adversaire);
+                                            //
+                                            break;
+                                        case 2:
+                                            echo "Vous avez choisi l'attaque spéciale Kamehameha." . PHP_EOL;
+                                            $this->attaque($this->adversaire);
+                                            //
+                                            break;
+                                        case 3:
+                                            echo "Vous avez choisi l'attaque spéciale Genkidama." . PHP_EOL;
+                                            $this->attaque($this->adversaire);
+                                            //
+                                            break;
+                                    //
+                                    break;
+                                    $adversaire->choiceAboutActions();
+                            }
+                            break;
+                        }
+                    break;
+                    //
+            case "q":
+                echo "Vous avez quitté le jeu.";
+                exit();
+            default:
+                echo "Veuillez choisir une action valide." . PHP_EOL;
+                enJeu();
+                break;
+        //endswitch;
+                }
+        }
+    }        //est_vivant();
+        
+    }
+
+    public function systeme_de_combat(){
+        $this->adversaire = $adversaire;
+        $this->nom = $nom;
+        $this->vie = $vie;
+        $this->puissance_attaque = $puissance_attaque;
+        $this->degats_subis = $degats_subis;
+        $this->adversaire = $adversaire;
+
+
+        //menu();
+        //synopsis(); if play puis choixCamp();
+
+        choixCamp();
+        enJeu();
+        
+    }
+}
+
+
+class IA extends Personnage{
+
+    protected $adversaire;
+    protected $nom;
+    protected $vie;
+    protected $puissance_attaque;
+    protected $degats_subis;
+
+    public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain){
+        parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+        $this->est_vilain = $est_vilain;
+    }
+
+    public function logiqueEnnemi(){
+        if($this->est_vilain == true){
+            $hero = new Heros($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+            $this->adversaire = $hero;
+        }
+        else{
+            $vilain = new Vilains($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+            $this->adversaire = $vilain;
+        }
+    }
+
+    public function logiqueCombat(){
+        while($this->vie > 0 && $this->adversaire->vie > 0){
+                        
+            $this->attaque($this->adversaire);
+            $this->adversaire->attaque($this);
+
+            $this->attaque($this->adversaire) == $this->prendre_degats($this->degats_subis);
+            $this->adversaire->attaque($this) == $this->adversaire->prendre_degats($this->degats_subis);
+
+            est_mort();
+        }
+    }
+
+    public function choiceAboutActions(){
+        $random = rand(0, 2);
+        $action = [$attaque, $transformation, $fuite];
+        $choixFinal = $action[$random];
+        return "$nom a choisi l'option $choixFinal";
+    }
+}
+
+// class Jeu extends combat{
+    
+//         private $adversaire;
+//         private $nom;
+//         private $vie;
+//         private $puissance_attaque;
+//         private $degats_subis;
+//         private $est_vilain;
+//         private $est_heros;
+    
+//         public function __construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire, $est_vilain, $est_heros){
+//             parent::__construct($nom, $vie, $puissance_attaque, $degats_subis, $adversaire);
+//             $this->est_vilain = $est_vilain;
+//             $this->est_heros = $est_heros;
+//         }
+    
+//         public function systeme_de_jeu(){
+//             $this->adversaire = $adversaire;
+//             $this->nom = $nom;
+//             $this->vie = $vie;
+//             $this->puissance_attaque = $puissance_attaque;
+//             $this->degats_subis = $degats_subis;
+//             $this->adversaire = $adversaire;
+//             $this->est_vilain = $est_vilain;
+//             $this->est_heros = $est_heros;
+
+//         }
+// }
+
+?>
